7729981ce0ad4c1e906813186135513317b6bf24
Iva Petkova <iva.g.petkova@abv.bg>
2019-03-16 20:39:16 +0200

    feat: Add header structure

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="container">
+        <Header></Header>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+    import Header from './components/Header.vue'
 
     export default {
-
+        components: {
+          Header
+        }
     }
 </script>
 
