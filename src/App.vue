d44036ebf0d9c3e03859f245b9e588b6aff1c90b
Iva Petkova <iva.g.petkova@abv.bg>
2019-03-19 23:46:07 +0200

    fix: Set demo stock data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="container">
         <Header></Header>
-        <router-view></router-view>
+        <div class="row">
+          <div class="col-xs-12">
+            <router-view></router-view>
+          </div>
+        </div>
     </div>
 </template>
 
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,5 +1,25 @@
 <template>
     <div class="component-container">
-        <h1>The Stocks Component</h1>
+        <Stock v-for="stock in stocks" :stock="stock"></Stock>
     </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue'
+
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'glasses', price: 190 },
+          { id: 2, name: 'phone', price: 1100 },
+          { id: 3, name: 'watch', price: 610 },
+          { id: 4, name: 'tablet', price: 10 },
+        ]
+      }
+    },
+    components: {
+      Stock
+    },
+  }
+</script>
